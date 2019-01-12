5faf51c2da47ec8a0bf41cb30df70bd34630db03
Cube <cube-root@outlook.com>
2019-01-12 20:10:45 +0800

    图片库改进

--- JavaScript/DOM/script/showpic.js
+++ JavaScript/DOM/script/showpic.js
@@ -1,8 +1,8 @@
-function showPic(whichpic){
+function showPic(whichpic) {
     let placeholder = document.getElementById("placeholder");
     // 改变placeholder的src属性值
-    placeholder.setAttribute("src",whichpic.getAttribute("href"));
-    placeholder.setAttribute("title",whichpic.getAttribute("title"));
+    placeholder.setAttribute("src", whichpic.getAttribute("href"));
+    placeholder.setAttribute("title", whichpic.getAttribute("title"));
 
     // 改变文本值
     let description = document.getElementById("description");
@@ -10,11 +10,34 @@ function showPic(whichpic){
     description.firstChild.nodeValue = text;
 }
 
+function prepareLinks() {
+    let links = document.getElementsByTagName("a");
+    for (let i = 0; i < links.length; i++) {
+        if (links[i].getAttribute("class") == "popup") {
+            links[i].onclick = function(){
+                return showPic(this)?false:true;
+            }
+        }
+    }
+}
+
 function showBody() {
     let body_element = document.getElementsByTagName("body")[0];
     alert[body_element];
-    for(let index = 0; index < body_element.childNodes.length;index++){
+    for (let index = 0; index < body_element.childNodes.length; index++) {
         console.log(body_element.childNodes[index].nodeValue);
     }
 }
-window.onload = showBody;
+
+function addLoadEvent(func){
+    let oldonload = window.onload;
+    if(typeof window.onload != 'function'){
+        window.onload = func;
+    }else{
+        oldonload();
+        func();
+    }
+}
+
+addLoadEvent(prepareLinks);
+addLoadEvent(showBody);
